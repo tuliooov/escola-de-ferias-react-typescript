78bb31d7f80e5ee840b447d0470c299d6d45c1e2
Marco Túlio de Oliveira Valeriano <marcotuliovaleriano2014@gmail.com>
2022-01-27 10:20:24 -0300

    layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,19 @@
-import React, { ReactNode, useEffect } from "react";
-import App, { AppProps } from "next/app";
-import Head from "next/head";
-
+import type { NextPage } from 'next';
+import type { AppProps as NextAppProps } from "next/app";
+import Head from 'next/head';
+import { ReactNode, useEffect } from 'react';
+import React from 'react';
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "../styles/tailwind.css";
-import { ContextGlobalProvider } from "../components/contexts/ContextGlobal";
-import { NextPage } from "next";
+import { ContextGlobalProvider } from '../components/contexts/ContextGlobal';
+
+type AppProps<P = any> = {
+  pageProps: P;
+} & Omit<NextAppProps<P>, "pageProps">;
 
 
-function MeuAppSw({ Component }): JSX.Element {
+function MeuAppSw({ Component }) {
   useEffect(() => {
     if("serviceWorker" in navigator) {
       window.addEventListener("load", function () {
@@ -28,14 +32,11 @@ function MeuAppSw({ Component }): JSX.Element {
   return Component
 }
 
-const defaultGetLayout: Layout = (page: ReactNode): ReactNode => page;
-
-
-type Layout = (page: ReactNode) => ReactNode;
+type GetLayout = (page: ReactNode) => ReactNode;
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 type Page<P = {}, IP = P> = NextPage<P, IP> & {
-  layout?: Layout;
+  getLayout?: GetLayout;
 };
 
 // eslint-disable-next-line @typescript-eslint/ban-types
@@ -43,28 +44,22 @@ type MyAppProps<P = {}> = AppProps<P> & {
   Component: Page<P>;
 };
 
-export default class MyApp extends App {
+const defaultGetLayout: GetLayout = (page: ReactNode): ReactNode => page;
 
-  render() {
-    const { Component, pageProps } = this.props as AppProps & MyAppProps;
-  
-    const Layout = Component.layout || defaultGetLayout;
+export default function App({ Component, pageProps }: MyAppProps): JSX.Element {
+  const getLayout = Component.getLayout ?? defaultGetLayout;
 
-    return (
-      <ContextGlobalProvider>
-          <Head>
-            <link rel="manifest" href="/manifest.json" />
-            <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1, shrink-to-fit=no"
-            />
-            <title>Escola de Ferias</title>
-            {/* <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script> */}
-          </Head>
-          <Layout>
-              <MeuAppSw Component={<Component {...pageProps} />}/>
-          </Layout>
-      </ContextGlobalProvider>
-    );
-  }
+  return (
+    <ContextGlobalProvider>
+      <Head>
+      <link rel="manifest" href="/manifest.json" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          <title>Escola de Ferias</title>
+      </Head>
+      {getLayout(<MeuAppSw Component={<Component {...pageProps} />}/>)}
+    </ContextGlobalProvider>
+  )
 }
